69abb8292f10149f8717c98c31ef4647712c5487
ginalam <Lam.gina510@gmail.com>
2019-10-16 01:37:09 +0000

    add click handler

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 $( "document" ).ready(function() {
 
+$("#button").click(function(){
+    var inputValue = $('.input').val();
+$(".output").text(inputValue)
+    console.log(inputValue)
 
 
-});
+function checkVowel (str){
+    var firstLetter= str [0];
+    var vowels = ['a','e','i','o','u'];
+
+    if (vowels.include(firstLetter)){
+        return 'starts with vowel';
+    }
+    return 'starts with consonant';
+    }
+})
+
+})
+
+
